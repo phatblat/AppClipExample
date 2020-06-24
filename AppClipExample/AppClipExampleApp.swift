02989bf057ea51ecce7f806be1ec63d0331a67d3
Ben Chatelain <ben@octop.ad>
2020-06-23 20:49:49 -0600

    🎉 Add iOS app template

---
+++ AppClipExample/AppClipExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipExampleApp.swift
+//  AppClipExample
+//
+//  Created by Ben Chatelain on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct AppClipExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
