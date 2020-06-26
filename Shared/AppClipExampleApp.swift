7b6bc6b30f7a9ba960080a4b216fa430fb8bcb80
Ben Chatelain <ben@octop.ad>
2020-06-26 12:16:14 -0600

    🚚 Xcode multiplatform template - macOS

---
+++ Shared/AppClipExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipExampleApp.swift
+//  Shared
+//
+//  Created by Ben Chatelain on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct AppClipExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
