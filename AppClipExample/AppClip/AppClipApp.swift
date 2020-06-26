8fda206ccbd729d99820c9618b4978cdd0355b5b
Ben Chatelain <ben@octop.ad>
2020-06-25 20:57:55 -0600

    🎯 Add AppClip target

---
+++ AppClipExample/AppClip/AppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppClipApp.swift
+//  AppClip
+//
+//  Created by Ben Chatelain on 6/25/20.
+//
+
+import SwiftUI
+
+@main
+struct AppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
